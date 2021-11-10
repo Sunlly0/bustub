eeb38b81d221c0ef1dea8807ccbb1ad5a5501afa
Sunlly0 <54405269+Sunlly0@users.noreply.github.com>
2021-11-10 17:18:12 +0800

    2021.11.10 执行器task1：catalog完成，测试通过

--- src/include/catalog/catalog.h
+++ src/include/catalog/catalog.h
@@ -77,14 +77,42 @@ class Catalog {
    */
   TableMetadata *CreateTable(Transaction *txn, const std::string &table_name, const Schema &schema) {
     BUSTUB_ASSERT(names_.count(table_name) == 0, "Table names should be unique!");
-    return nullptr;
+    //1.创建新表（TableMetaData），分配table_oid
+    TableHeap *tableheap=new TableHeap(bpm_,lock_manager_,log_manager_,txn);
+    auto table_oid=next_table_oid_++;
+    auto *tablemetadata=new TableMetadata(schema,table_name,static_cast<std::unique_ptr<TableHeap>>(tableheap),table_oid);
+
+    //2.添加到hash表：tables_（table_oid->tablementadata）
+    tables_[table_oid]=static_cast<std::unique_ptr<TableMetadata>>(tablemetadata);
+
+    //3.添加到hash表：names_（name->table_oid）
+    names_[table_name]=table_oid;
+    return tablemetadata;
   }
 
   /** @return table metadata by name */
-  TableMetadata *GetTable(const std::string &table_name) { return nullptr; }
+  TableMetadata *GetTable(const std::string &table_name) {
+    //从hash表：names_中找table_oid，再从tables_中找table_meta_data
+    if(names_.count(table_name)==0) {
+      throw std::out_of_range(table_name);
+    }
+    auto table_oid=names_[table_name];
+    if(tables_[table_oid]!=0){
+      //Q：为啥要加get才行呢？
+      return tables_[table_oid].get();
+    }
+    return nullptr;
+  }
 
   /** @return table metadata by oid */
-  TableMetadata *GetTable(table_oid_t table_oid) { return nullptr; }
+  TableMetadata *GetTable(table_oid_t table_oid) {
+    //从hash表：tables_中找table_meta_data
+    if(tables_[table_oid]!=0){
+      //Q：同上
+      return tables_[table_oid].get();
+    }
+    return nullptr;
+ }
 
   /**
    * Create a new index, populate existing data of the table and return its metadata.
@@ -101,14 +129,57 @@ class Catalog {
   IndexInfo *CreateIndex(Transaction *txn, const std::string &index_name, const std::string &table_name,
                          const Schema &schema, const Schema &key_schema, const std::vector<uint32_t> &key_attrs,
                          size_t keysize) {
+    //1.创建新索引（IndexMetadata）（Index）（IndexInfo），分配index_oid
+    auto index_oid=next_index_oid_++;
+    auto *indexmetadata=new IndexMetadata(index_name,table_name,&schema,key_attrs);
+    auto *index=new BPlusTreeIndex(new BPlusTreeIndex<KeyType,ValueType,KeyComparator>(indexmetadata,bpm_));
+    auto *indexinfo=new IndexInfo(key_schema,index_name,static_cast<std::unique_ptr<Index>>(index),index_oid,table_name,keysize);
+
+    //2.添加到hash表：indexes_（index_oid->indexmentadata）
+    indexes_[index_oid]=static_cast<std::unique_ptr<IndexInfo>>(indexinfo);
+
+    //3.添加到hash表：index_names_（table_name->index_name,_index_oid）
+    index_names_[table_name].insert(std::unordered_map<std::string, index_oid_t>::value_type (index_name, index_oid));
+
+    //TODO: add index for every tuple??
+
+    return indexinfo;
+  }
+
+  IndexInfo *GetIndex(const std::string &index_name, const std::string &table_name) {
+    //从hash表：index_names_中找index_oid，再从indexes_中找index_info
+    if(index_names_.count(table_name)==0) {
+      throw std::out_of_range(table_name);
+    }
+    auto index_oid=index_names_[table_name][index_name];
+    if(indexes_[index_oid]!=0){
+      return indexes_[index_oid].get();
+    }
+    return nullptr;
+}
+
+  IndexInfo *GetIndex(index_oid_t index_oid) {
+    //从hash表：indexes_中找index_info
+    if(indexes_[index_oid]!=0){
+      return indexes_[index_oid].get();
+    }
     return nullptr;
   }
 
-  IndexInfo *GetIndex(const std::string &index_name, const std::string &table_name) { return nullptr; }
-
-  IndexInfo *GetIndex(index_oid_t index_oid) { return nullptr; }
-
-  std::vector<IndexInfo *> GetTableIndexes(const std::string &table_name) { return std::vector<IndexInfo *>(); }
+  std::vector<IndexInfo *> GetTableIndexes(const std::string &table_name) {
+    if(index_names_.count(table_name)==0) {
+      throw std::out_of_range(table_name);
+    }
+    //用vector保存index_info
+    std::vector<IndexInfo*> index_vector;
+    //先在index_names_中找到table中所有的index_oid，再在indexes_中找到index_info.
+    for (auto &pair : index_names_[table_name]) {
+      auto index_oid=pair.second;
+//      index_vector.push_back(static_cast<IndexInfo *>(indexes_[index_oid].get()));
+      index_vector.push_back(indexes_[index_oid].get());
+    }
+    return index_vector;
+  }
 
  private:
   [[maybe_unused]] BufferPoolManager *bpm_;
