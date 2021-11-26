bfcbcd5dac01ea79a18dda5248cd3d9c95cc984a
Sunlly0 <54405269+Sunlly0@users.noreply.github.com>
2021-11-26 20:45:18 +0800

    2021.11.25 各executor完成修改

--- src/execution/nested_index_join_executor.cpp
+++ src/execution/nested_index_join_executor.cpp
@@ -38,7 +38,7 @@ bool NestIndexJoinExecutor::Next(Tuple *tuple, RID *rid) {
         left_tuple.KeyFromTuple(*(plan_->OuterTableSchema()), index_info_->key_schema_, index_->GetKeyAttrs());
     index_->ScanKey(index_key, &rid_index, exec_ctx_->GetTransaction());
     if (rid_index.size() > 0) {
-      bool res = table_heap_->GetTuple(rid_index.at(0), &right_tuple, exec_ctx_->GetTransaction());
+      table_heap_->GetTuple(rid_index.at(0), &right_tuple, exec_ctx_->GetTransaction());
       std::vector<Value> left_values = GetAllValues(&left_tuple, plan_->OuterTableSchema());
       std::vector<Value> right_values = GetAllValues(&right_tuple, plan_->InnerTableSchema());
       left_values.insert(left_values.end(), right_values.begin(), right_values.end());
